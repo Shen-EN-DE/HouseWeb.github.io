f56266b98b8ce316eb7e7e5852e82ef011f69565
unknown <joy891988@gmail.com>
2024-02-10 17:31:26 +0800

    13OK

--- src/components/Footer/FooterItem/ContractInfo.js
+++ src/components/Footer/FooterItem/ContractInfo.js
@@ -6,7 +6,7 @@ const ContactInfo = () => {
       <h4>聯絡我們</h4>
       <p>電話: xxx-xxx-xxxx</p>
       <p>郵件: info@example.com</p>
-      <p>地址: xxxxx街道xxxx號</p>
+      <p>地址: 台北市忠孝東路4段270號17樓</p>
     </div>
   );
 };
